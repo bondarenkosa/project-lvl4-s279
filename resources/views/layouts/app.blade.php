825737b7c2e1d834c920d9592606f1baa1fc0cd8
Sergei Bondarenko <andbondsa@gmail.com>
2018-09-07 11:43:24 +0500

    add link to user menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('account.edit') }}">
+                                        {{ __('Edit account') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
