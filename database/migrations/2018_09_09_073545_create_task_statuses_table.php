6c7cf1b01d59fe4ca3ea76f38e59a1b789044924
Sergei Bondarenko <andbondsa@gmail.com>
2018-09-10 16:58:21 +0500

    fix task statuses migration

--- database/migrations/2018_09_09_073545_create_task_statuses_table.php
+++ database/migrations/2018_09_09_073545_create_task_statuses_table.php
@@ -15,7 +15,7 @@ class CreateTaskStatusesTable extends Migration
     {
         Schema::create('task_statuses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable(false);
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
