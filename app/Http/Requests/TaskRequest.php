ab22dd288064261204a15404853aff2247a51d61
Sergei Bondarenko <andbondsa@gmail.com>
2018-09-11 16:05:28 +0500

    add tasks validation rules

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'string',
+            'status' => 'required|string|exists:task_statuses',
+            'executor_id' => 'required|integer|exists:users,id',
+        ];
+    }
+}
